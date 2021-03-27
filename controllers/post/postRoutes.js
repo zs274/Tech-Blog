989751c07811c50e3247adc4f914b6f55f26b886
Zarin Salim <zarin_96@hotmail.co.uk>
2021-03-27 01:27:19 +0000

    created main handlebars

--- controllers/post/postRoutes.js
+++ controllers/post/postRoutes.js
@@ -51,6 +51,50 @@ router.post('/new', withAuth, async (req, res) => {
     }
 });
 
+router.post('./edit/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.update(req.body, {
+            where: {
+                id: req.params.id
+            },
+        });
+
+        postData.description = req.body.description;
+        const writeFile = req.body.text;
+        const filename = req.body.filename;
+
+        writeToFile(writeFile, filename);
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post with this id' });
+            return;
+        }
+
+        postData.name = req.body.name;
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.delete('/edit/:id', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.destroy({
+            where: {
+                id: req.params.id
+            },
+        });
+
+        if (!postData) {
+            res.status(404).json({ message: 'No post with this id' });
+            return;
+        }
+
+        res.status(200).json(postData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 updateFilename = async (post) => {
     const updatedFilename = post.id;
@@ -65,3 +109,4 @@ updateFilename = async (post) => {
         console.log(err);
     }
 };
+
